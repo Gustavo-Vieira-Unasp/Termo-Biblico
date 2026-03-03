1b2d25b7098afc3f6607238d2254f30eb7e8edfb
Gustavo Vieira <fonseca_vieira@educadventista.org>
2026-03-03 00:56:56 -0300

    Novas palavras

--- docs/palavras.js
+++ docs/palavras.js
@@ -8,4 +8,12 @@ const listaDePalavras = [
     "JEJUM", "PECAR", "MONTE", "JUDAS", "JOSUE", "FARAO", "RAMOS", "POMBA", 
     "TENDA", "BABEL", "CESAR", "MOABE", "SANTO", "OBRAS", "FESTA", "HINOS", 
     "ANIMO", "AMIGO", "VOZES", "SAULO", "SILAS", "NOEMI", "BETEL",
+    // Novas palavras que podem ser adicionadas
+    "ISAAC", "SALVO", "PREGO", "MORTE", "SIMAO", "EGITO", "ALTAR", "MIRRA",
+    "FRUTO", "FORMA", "VAZIA", "CANTO", "MOEDA", "FIGOS", "TABUA", "PRESO",
+    "LEPRA", "SAPOS", "CHORO", "FILHO", "CRIOU", "CARTA", "TRIGO", "PERSA",
+    "MEDOS", "SIRIA", "NUVEM", "LIVRO", "VIVER", "IRMAO", "OLIVA", "JUDEU",
+    "CESTO", "CHUVA", "SARAI", "ABRAO", "PORCO", "HARPA", "TOCAR", "LEOES",
+    "OSSOS", "POVOS", "TRIBO", "FUNDA", "PEDRA", "PESCA", "VIVOS", "VIUVA",
+    "LANCA", "LAVAR", "COMER", "DIABO", "TEMPO", "NATAM", "BARCO",
 ];
